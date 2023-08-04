f40e56f9654fe48c15d76969b13d6da4e94363ca
pasupuletihemu <hemanthhemu769@gmail.com>
2023-08-04 16:01:17 +0530

    Add project files.

---
+++ polymorphism/MyLib.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class MyLib
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine("int int" +"   "+(a+b));
+        }
+        public static void Add(int a, int b,int c)
+        {
+            Console.WriteLine("int int int" +"   "+ (a + b+c));
+        }
+        public static void Add(long a, long b)
+        {
+            Console.WriteLine("long long" +"   "+ (a + b));
+        }
+        public static void Add(float a, float b)
+        {
+            Console.WriteLine("float float" +"    "+ (a + b));
+        }
+        public static void Add(string a, string b)
+        {
+            Console.WriteLine("string string" +"   "+ (a + b));
+        }
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine("double double" +"    "+ (a + b));
+        }
+    }
+}
---
+++ polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLib.Add(199, 99);
+            MyLib.Add(199, 1, 99);
+            MyLib.Add("hemanth","hemu");
+            MyLib.Add(999.18,76.98);
+            Console.WriteLine(int.MaxValue);
+            MyLib.Add(2147483648, 2147483649);
+            MyLib.Add(19L, 29L);
+            MyLib.Add(19.9f, 26.99f);
+
+        }
+    }
+}
